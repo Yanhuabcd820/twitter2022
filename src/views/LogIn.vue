7fadca2f712d228744eb29658a5f60ad3a13f2ba
Bunreal <bunreal0@gmail.com>
2022-05-10 06:54:48 +0800

    update login page without css

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    login page
+  <div class="container">
+    <form action="">
+      <div>
+        <img src="./../assets/icons/logo@2x.png" alt="">
+        <h1>登入 Alphitter</h1>
+      </div>
+      <div class="form-label-group mb-2">
+        <label for="account">帳號</label>
+        <input
+          id="account"
+          name="account"
+          type="text"
+          placeholder="請輸入帳號"
+          required
+          autofocus
+          v-model="account"
+        >
+      </div>
+      <div class="form-label-group mb-2">
+        <label for="account">密碼</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="請輸入密碼"
+          required
+          autofocus
+          v-model="password"
+        >
+      </div>
+    </form>
   </div>
 </template>

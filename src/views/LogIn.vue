b5839644510a33362905244cf87a9ba2f724eb01
Bunreal <bunreal0@gmail.com>
2022-05-10 22:40:34 +0800

    add regist.vue , made css file to link, modify login.vue

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -35,88 +35,12 @@
         <p>登入</p>
       </div>
       <nav>
-        <a href="">註冊</a>
+        <router-link to="/regist">註冊</router-link>
         <p>・</p>
-        <a href="">後台登入</a>
+        <router-link href="">後台登入</router-link>
       </nav>
     </form>
   </div>
 </template>
 
-<style scoped>
-* {
-  padding: 0px;
-  margin: 0px;
-}
-
-.container {
-  position: relative;
-}
-form {
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-}
-.logo{
-  margin-top: 69px;
-  display: flex;
-  justify-content: center;
-}
-img {
-  width: 50px;
-  height: 50px;
-}
-.title {
-  margin-top: 29px;
-  margin-bottom: 40px;
-  text-align: center;
-}
-.title h3 {
-  font-size: 28px;
-}
-.form-label-group {
-  display: flex;
-  flex-direction: column;
-  width: 356px;
-  background-color: #F5F8FA;
-  border-bottom: 2px #657786 solid;
-  padding: 2px 10.55px;
-  margin-bottom: 32px;
-  box-sizing: border-box;
-}
-label{
-  font-size: 14px;
-  line-height: 22px;
-}
-input{
-  font-size: 16px;
-  line-height: 26px;
-  border: 0;
-  background-color: #F5F8FA;
-  outline: none;
-}
-.confirm-btn {
-  background-color: #FF6600;
-  width: 356px;
-  height: 46px;
-  border-radius: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 20px;
-  line-height: 30px;
-  color: white;
-}
-nav {
-  margin-top: 22px;
-  display: flex;
-  justify-content: end;
-}
-nav > * {
-  margin-left: 12px;
-}
-nav:last-child {
-  margin-right: 15px;
-}
-
-</style>
+<style lang="css" src="./../assets/css/LoginRegist.css" scoped></style>
---
+++ src/views/Regist.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container">
+    <form action="">
+      <div class="logo">
+        <img src="./../assets/icons/logo@2x.png" alt="">
+      </div>
+      <div class="title">
+        <h3>建立你的稱號</h3>
+      </div>
+      <div class="form-label-group">
+        <label for="account">帳號</label>
+        <input
+          id="account"
+          name="account"
+          type="text"
+          placeholder="請輸入帳號"
+          required
+          autofocus
+          v-model="account"
+        >
+      </div>
+      <div class="form-label-group">
+        <label for="name">名稱</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="請輸入密碼"
+          required
+          autofocus
+          v-model="name"
+        >
+      </div>
+      <div class="form-label-group">
+        <label for="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="請輸入密碼"
+          required
+          autofocus
+          v-model="email"
+        >
+      </div>
+      <div class="form-label-group">
+        <label for="password">密碼</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="請輸入密碼"
+          required
+          autofocus
+          v-model="password"
+        >
+      </div>
+      <div class="form-label-group">
+        <label for="password_confirm">密碼確認</label>
+        <input
+          id="password_confirm"
+          name="password_confirm"
+          type="password"
+          placeholder="請輸入密碼"
+          required
+          autofocus
+          v-model="password_confirm"
+        >
+      </div>
+      <div class="confirm-btn">
+        <p>註冊</p>
+      </div>
+      <nav>
+        <router-link to="/login">取消</router-link>
+      </nav>
+    </form>
+  </div>
+</template>
+
+<style lang="css" src="./../assets/css/LoginRegist.css" scoped></style>
+<style scoped>
+  nav {
+    justify-content: center;
+  }
+</style>

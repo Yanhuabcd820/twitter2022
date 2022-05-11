761f87cf60895f7a9c831522ebb0d9cdd51c329b
Yanhuabcd820 <margaret0820@yahoo.com.tw>
2022-05-11 16:54:02 +0800

    mainPage_css

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <main 
-      role="main"
-      class="mt-5 bg-white"
-    >
-      <router-view/>
+    <main role="main" class="mt-5 bg-white">
+      <router-view />
     </main>
   </div>
 </template>
+
+
+<style lang="css" src="@/assets/css/reset.css" ></style>
+<style lang="css" src="@/assets/css/global.css" ></style>
---
+++ src/components/nav.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nav">
+    <div class="logo">
+      <img src="../assets/images/logo.png" alt="" />
+    </div>
+    <div class="nav-btn-group">
+      <a href="#" class="nav-btn">
+        <div class="nav-btn-img nav-btn-home-img"></div>
+        <h5>首頁</h5>
+      </a>
+      <a href="#" class="nav-btn">
+        <div class="nav-btn-img nav-btn-user-img"></div>
+        <h5>個人資料</h5>
+      </a>
+      <a href="#" class="nav-btn">
+        <div class="nav-btn-img nav-btn-setting-img"></div>
+        <h5>設定</h5>
+      </a>
+    </div>
+    <a class="btn nav-btn-tweet" href="#"> 推文 </a>
+    <div class="nav-btn-group-bottom">
+      <div class="nav-btn-group btn-bottom">
+        <a href="#" class="nav-btn">
+          <div class="nav-btn-img nav-btn-left-img"></div>
+          <h5>登出</h5>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="css" src="@/assets/css/nav.css" scoped></style>

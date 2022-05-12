adf840d558c6e1804739dab4e87574809fc8e8ed
Bunreal <bunreal0@gmail.com>
2022-05-12 21:20:08 +0800

    do probs in userinfo from selfpage

--- src/views/SelfPage.vue
+++ src/views/SelfPage.vue
@@ -3,7 +3,7 @@
     <navigation />
     <div class="main">
       <userTitle />
-      <userInfo />
+      <userInfo :user="user"/>
       <navTabs />
       <div class="tweet-wrap">
         <div class="tweet-card">
@@ -70,134 +70,6 @@
             </div>
           </div>
         </div>
-        <div class="tweet-card">
-          <div class="tweet-avatar">
-            <img src="../assets/images/avatar_default.png" alt="" />
-          </div>
-          <div class="tweet-content">
-            <div class="tweet-name-group">
-              <p class="tweet-name"><b>Apple</b></p>
-              <p class="tweet-account fz14">@apple・3 小時</p>
-            </div>
-            <div class="tweet-text">
-              <p>
-                Nulla Lorem mollit cupidatat irure. Laborum magna nulla
-                dudsdsdsdsis ullamco cillum dolor. Voluptate exercitation
-                incididunt aliquip deserunt reprehenderit elit laborum.
-              </p>
-            </div>
-            <div class="tweet-count">
-              <a href="#" class="tweet-reply">
-                <div class="tweet-reply-img">
-                  <img src="../assets/images/tweet-reply.png" alt="" />
-                </div>
-                <p class="fz14"><b>13</b></p>
-              </a>
-              <a href="#" class="tweet-like">
-                <div class="tweet-like-img">
-                  <img src="../assets/images/tweet-like.png" alt="" />
-                </div>
-                <p class="fz14"><b>76</b></p>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="tweet-card">
-          <div class="tweet-avatar">
-            <img src="../assets/images/avatar_default.png" alt="" />
-          </div>
-          <div class="tweet-content">
-            <div class="tweet-name-group">
-              <p class="tweet-name"><b>Apple</b></p>
-              <p class="tweet-account fz14">@apple・3 小時</p>
-            </div>
-            <div class="tweet-text">
-              <p>
-                Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
-                ullamco cillum dolor. Voluptate exercitation incididunt aliquip
-                deserunt reprehenderit elit laborum.
-              </p>
-            </div>
-            <div class="tweet-count">
-              <a href="#" class="tweet-reply">
-                <div class="tweet-reply-img">
-                  <img src="../assets/images/tweet-reply.png" alt="" />
-                </div>
-                <p class="fz14"><b>13</b></p>
-              </a>
-              <a href="#" class="tweet-like">
-                <div class="tweet-like-img">
-                  <img src="../assets/images/tweet-like.png" alt="" />
-                </div>
-                <p class="fz14"><b>76</b></p>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="tweet-card">
-          <div class="tweet-avatar">
-            <img src="../assets/images/avatar_default.png" alt="" />
-          </div>
-          <div class="tweet-content">
-            <div class="tweet-name-group">
-              <p class="tweet-name"><b>Apple</b></p>
-              <p class="tweet-account fz14">@apple・3 小時</p>
-            </div>
-            <div class="tweet-text">
-              <p>
-                Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
-                ullamco cillum dolor. Voluptate exercitation incididunt aliquip
-                deserunt reprehenderit elit laborum.
-              </p>
-            </div>
-            <div class="tweet-count">
-              <a href="#" class="tweet-reply">
-                <div class="tweet-reply-img">
-                  <img src="../assets/images/tweet-reply.png" alt="" />
-                </div>
-                <p class="fz14"><b>13</b></p>
-              </a>
-              <a href="#" class="tweet-like">
-                <div class="tweet-like-img">
-                  <img src="../assets/images/tweet-like.png" alt="" />
-                </div>
-                <p class="fz14"><b>76</b></p>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="tweet-card">
-          <div class="tweet-avatar">
-            <img src="../assets/images/avatar_default.png" alt="" />
-          </div>
-          <div class="tweet-content">
-            <div class="tweet-name-group">
-              <p class="tweet-name"><b>Apple</b></p>
-              <p class="tweet-account fz14">@apple・3 小時</p>
-            </div>
-            <div class="tweet-text">
-              <p>
-                Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
-                ullamco cillum dolor. Voluptate exercitation incididunt aliquip
-                deserunt reprehenderit elit laborum.
-              </p>
-            </div>
-            <div class="tweet-count">
-              <a href="#" class="tweet-reply">
-                <div class="tweet-reply-img">
-                  <img src="../assets/images/tweet-reply.png" alt="" />
-                </div>
-                <p class="fz14"><b>13</b></p>
-              </a>
-              <a href="#" class="tweet-like">
-                <div class="tweet-like-img">
-                  <img src="../assets/images/tweet-like.png" alt="" />
-                </div>
-                <p class="fz14"><b>76</b></p>
-              </a>
-            </div>
-          </div>
-        </div>
       </div>
 
     </div>
@@ -211,8 +83,26 @@ import userInfo from "../components/userInfo";
 import userTitle from "../components/userTitle";
 import navTabs from "../components/navTabs";
 
+// 要得到使用者info、使用者自己的推文、推計追蹤者的資料
+// 使用者info丟進去component，使用者自己的推文直接render
+const dummyUser = {
+  "id": 1,
+  "account": "heyjohn",
+  "name": "John Doe",
+  "email": "root@example.com",
+  "role": "admin",
+  "introduction": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+  "avatar": "../assets/images/AvatarBigger.png",
+  "cover": "../assets/images/cover.png",
+  "followingCount": 34,
+  "followerCount": 59,
+  "isFollowing": true,
+  "createdAt": "2022-01-18T07:23:18.000Z",
+  "updatedAt": "2022-01-18T07:23:18.000Z"
+}
+
 export default {
-  name: "mainPage",
+  name: "selfPage",
   components: {
     navigation,
     followTop,
@@ -221,8 +111,33 @@ export default {
     navTabs
   },
   data() {
-    return {};
+    return {
+      user: {
+        id: -1,
+        account: "",
+        name: "",
+        email: "",
+        role: "",
+        introduction: "",
+        avatar: "",
+        cover: "",
+        followingCount: -1,
+        followerCount: -1,
+        isFollowing: false,
+        createdAt: "",
+        updatedAt: ""
+      }
+    };
   },
+  methods: {
+    fetchUser(){
+      const {id,account,name,email,role, introduction, avatar,cover,followingCount,followerCount,isFollowing,createdAt,updatedAt} = dummyUser
+      this.user = {id,account,name,email,role, introduction, avatar,cover,followingCount,followerCount,isFollowing,createdAt,updatedAt}
+    }
+  },
+  created(){
+    this.fetchUser()
+  }
 };
 </script>
 
